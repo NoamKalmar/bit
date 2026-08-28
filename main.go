825420e45fe96dce95c7965e97bc4f5908b61c00
NoamKalmar <noamkalmar@gmail.com>
2026-08-28 15:47:44 +0300

    added the init command that lets you initalize the bit project

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const defaultBranch = "main"
+
+func CreateDirs(paths []string) error {
+	for _, path := range paths {
+		err := os.MkdirAll(path, 0755)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func InitMainDir() error {
+	paths := []string{
+		".bit/objects/commits",
+		".bit/objects/trees",
+		".bit/objects/blobs",
+		".bit/branches",
+	}
+	err := CreateDirs(paths)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(".bit/head")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.WriteString(defaultBranch)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	command := os.Args[1]
+	switch command {
+	case "init":
+		err := InitMainDir()
+		if err != nil {
+			fmt.Println("Error while tring to init project: " + err.Error())
+		}
+		return
+	default:
+		fmt.Println("Unknown command: " + command)
+		return
+	}
 }
